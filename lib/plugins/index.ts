af97f30eedcb5fc7425f26d9b17cfd7e4bf581cc
DaXcess <daxcess@daxcess.io>
2023-02-22 21:41:18 +0100

    Cry about it

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -1,20 +1,20 @@
-import { NodeCoreBase, NodeCorePluginClient } from "../core/client";
+import { NodeCoreClient, NodeCorePluginClient } from "../core/client";
 import { PayloadLike } from "../datatypes/common";
 import * as core from "./core";
-import * as management from './management';
+import * as management from "./management";
 
 interface Plugin {
-    name: string,
-    onPacket?: (client: NodeCorePluginClient<NodeCoreBase>, pipe: string, payload: PayloadLike[]) => boolean
+  name: string;
+  onPacket?: (client: NodeCorePluginClient<NodeCoreClient>, pipe: string, payload: PayloadLike[]) => boolean;
 }
 
-export const PLUGINS: {[key: string]: Plugin} = {
-    'd4466edc-d84f-f2d0-8dce-eb4345fd8569': {
-        name: 'core',
-        onPacket: core.onPacket
-    },
-    '8e554d9c-a2bd-1b48-e703-c5704de5a7d8': {
-        name: 'management',
-        onPacket: management.onPacket
-    }
-}
+export const PLUGINS: { [key: string]: Plugin } = {
+  "d4466edc-d84f-f2d0-8dce-eb4345fd8569": {
+    name: "core",
+    onPacket: core.onPacket,
+  },
+  "8e554d9c-a2bd-1b48-e703-c5704de5a7d8": {
+    name: "management",
+    onPacket: management.onPacket,
+  },
+};
